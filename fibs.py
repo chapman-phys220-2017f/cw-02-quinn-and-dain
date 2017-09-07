be162b2667e0a7086a889d94ce30fbc2e8201785
Quinn Gates <gates111@mail.chapman.edu>
2017-09-07 17:21:28 +0000

    Modified fibs.py program

--- fibs.py
+++ fibs.py
@@ -7,16 +7,24 @@ def fibs(n):
     elif n==2:
         return [1,1]
     elif n>=3:
-        for i in range(n-2):
-            fiblist.append(fiblist[n-2]+fiblist[n-1])
+        for i in range(2,n):
+            fiblist.append(fiblist[i-2]+fiblist[i-1])
         return fiblist
     else:
         print("Oopsies! not a valid input")
 
 def fib_generator():
-    
+    a = 1
+    b = 1
+    while True:
+        yield a
+        a,b = b,a+b
 
 if __name__ == "__main__":
     n = input("Enter a value for n: ")
     print("Fibonacci sequnce with n values")
     print(fibs(n))
+    print("Fibonacci generator")
+    g = fib_generator()
+    fiblist = [next(g) for _ in range(n)]
+    print(fiblist)
