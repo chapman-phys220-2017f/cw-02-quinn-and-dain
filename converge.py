80c72ec28ff4eee6e7ac4717cd4b257f48efad88
Quinn Gates <gates111@mail.chapman.edu>
2017-09-10 23:53:13 +0000

    Modified Converge and Coord

--- converge.py
+++ converge.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
 def compute_sum(tol=1e-2):
-    k=1
+    k=float(1)
     sum = 0 
-    while (1/k**2)>=tol:
-        sum+=(1/k**2)
+    while ((1/k/k)>=tol):
+        sum+=(1/k/k)
         k+=1
     return sum
 if __name__ == "__main__":
     print("Computing sum of infinite series:")
     print(compute_sum())
+    print("With tol=1e-5:")
+    print(compute_sum(1e-5))
     print("With tol=1e-10:")
     print(compute_sum(1e-10))
-    print("With tol=1e-100:")
-    print(compute_sum(1e-100))
         
